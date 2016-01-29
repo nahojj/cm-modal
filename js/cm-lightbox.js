3fceaccfd3391048acd411cf2e9291872d44156d
nahojj <johanssonjohan@outlook.com>
2016-01-29 20:25:32 +0100

    Issue bugs are done and css with example

--- js/cm-lightbox.js
+++ js/cm-lightbox.js
@@ -1,19 +1,17 @@
 (function() {
-    // This
-    var self = this;
-
     // Define our constructor
-    self.Modal = function() {
+    this.Modal = function() {
         // Create global element references
-        self.closeButton = null;
-        self.modal       = null;
-        self.overlay     = null;
+        this.closeButton = null;
+        this.modal       = null;
+        this.overlay     = null;
 
         // Determine proper prefix
-        self.transitionEnd = transitionSelect();
+        this.transitionEnd = transitionSelect();
 
         // Define option defaults
         var defaults = {
+            autoOpen: false,
             className: 'fade-and-drop',
             closeButton: true,
             content: "",
@@ -24,8 +22,10 @@
 
         // Create options by extending defaults with the passed in arguments
         if(arguments[0] && typeof arguments[0] === 'object') {
-            self.options = extendDefaults(defaults, arguments[0]);
+            this.options = extendDefaults(defaults, arguments[0]);
         }
+
+        if(this.options.autoOpen === true) this.open();
     }
 
     // Public Methods
@@ -35,15 +35,15 @@
         var _ = this;
 
         // Remove the open class
-        self.modal.className    = self.modal.className.replace('cm-modal-open', '');
-        self.overlay.className  = self.overlay.className.replace('cm-modal-open');
+        this.modal.className   = this.modal.className.replace(' cm-modal-open', '');
+        this.overlay.className = this.overlay.className.replace(' cm-modal-open','');
 
         // Listen for CSS transitionend evt and then remove the nodes from the DOM
-        self.modal.addEventListener(self.transitionEnd, function() {
+        this.modal.addEventListener(this.transitionEnd, function() {
             _.modal.parentNode.removeChild(_.modal);
-        });
+       });
 
-        self.overlay.addEventListener(self.transitionEnd, function() {
+        this.overlay.addEventListener(this.transitionEnd, function() {
             if(_.overlay.parentNode) _.overlay.parentNode.removeChild(_.overlay);
         });
     }
@@ -53,19 +53,17 @@
         buildOut.call(this);
 
         // Init event
-        initEvents.casll(this);
+        initEvents.call(this);
 
         // Then, we adding elements to the DOM. USe getComputedStyle
-        // force the broswer re recale and recognize the elements that we just added.
+        // force the broswer recale and recognize the elements that we just added.
         // Cause css animation has a start point.
-        window.getComputedStyle(self.modal).height;
+        window.getComputedStyle(this.modal).height;
 
         // Add our open class and check if the modal is taller than the window
         // If true, anchored class will be applied.
-        self.modal.className = self.modal.className +
-            (self.modal.offsetHeight > window.innerHeight ? 'cm-modal-open cm-modal-anchored') : 'cm-modal-open';
-
-        self.overlay.className = self.overlay.className + 'cm-modal-open';
+        this.modal.className = this.modal.className + (this.modal.offsetHeight > window.innerHeight ? ' cm-modal-open cm-modal-anchored ' : ' cm-modal-open');
+        this.overlay.className = this.overlay.className + ' cm-modal-open';
     }
 
     // Private Methods
@@ -76,35 +74,35 @@
         // If content is an HTML string, append the html string.
         // If content is a domNode, append its content.
 
-        if(typeof self.options.content === 'string') {
-            content = self.options.content;
+        if(typeof this.options.content === 'string') {
+            content = this.options.content;
         } else {
-            content = self.options.content.innerHTML;
+            content = this.options.content.innerHTML;
         }
 
         // Create a Documentframent to build with
         docFrag = document.createDocumentFragment();
 
         // Create modal element.
-        self.modal                  = document.createElement('div');
-        self.modal.className        = 'cm-modal ' + self.options.className;
-        self.modal.style.minWidth   = self.options.minWidth + 'px';
-        self.modal.style.maxWidth   = self.options.maxWidth + 'px';
+        this.modal                  = document.createElement('div');
+        this.modal.className        = 'cm-modal ' + this.options.className;
+        this.modal.style.minWidth   = this.options.minWidth + 'px';
+        this.modal.style.maxWidth   = this.options.maxWidth + 'px';
 
         // if closeButton option true, add close button
-        if(self.options.closeButton === true) {
-            self.closeButton           = document.createElement('button');
-            self.closeButton.className = 'cm-modal-close close-button';
-            self.closeButton.innerHTML = 'X';
-            this.modal.appendChild(self.closeButton);
+        if(this.options.closeButton === true) {
+            this.closeButton           = document.createElement('button');
+            this.closeButton.className = 'cm-modal-close close-button';
+            this.closeButton.innerHTML = 'X';
+            this.modal.appendChild(this.closeButton);
         }
 
         // if overlay is true, then we add it!
-        if(self.options.overlay === true) {
-            self.overlay           = document.createElement('div');
-            self.overlay.className = 'cm-modal-overlay ' + self.options.classname;
+        if(this.options.overlay === true) {
+            this.overlay           = document.createElement('div');
+            this.overlay.className = 'cm-modal-overlay ' + this.options.className;
 
-            docFrag.appendChild(self.overlay);
+            docFrag.appendChild(this.overlay);
         }
 
         // Create content area and append it to modal
@@ -112,10 +110,10 @@
         contentHolder.className = 'cm-modal-content';
         contentHolder.innerHTML = content;
 
-        self.modal.appendChild(contentHolder);
+        this.modal.appendChild(contentHolder);
 
         // Append modal to docFrag
-        docFrag.appendChild(self.modal);
+        docFrag.appendChild(this.modal);
 
         // Append to body
         document.body.appendChild(docFrag);
@@ -134,21 +132,35 @@
     }
 
     function initEvents() {
-        if(self.closeButton) {
-            self.closeButton.addEventListener('click', self.close.bind(this));
+        if(this.closeButton) {
+            this.closeButton.addEventListener('click', this.close.bind(this));
         }
 
-        if(self.overlay) {
-            self.overlay.addEventListener('click', self.close.bind(this));
+        if(this.overlay) {
+            this.overlay.addEventListener('click', this.close.bind(this));
         }
     }
 
     function transitionSelect() {
-        var el = document.createElement("div");
-        if (el.style.WebkitTransition) return "webkitTransitionEnd";
-        if (el.style.OTransition) return "oTransitionEnd";
+        var el = document.createElement('div');
+        if (el.style.WebkitTransition) return 'webkitTransitionEnd';
+        if (el.style.OTransition) return 'oTransitionEnd';
         return 'transitionend';
     }
+}());
+
+/*  EXAMPLE  */
+// ================
 
 
-})();
+var myContent = document.querySelector('.modal-example');
+
+var myModal = new Modal({
+  content: myContent
+});
+
+var triggerButton = document.getElementById('trigger');
+
+triggerButton.addEventListener('click', function() {
+    myModal.open();
+});
