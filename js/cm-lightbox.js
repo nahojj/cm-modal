a537d21d3c8bad409613e59ccce787af68495b87
nahojj <johanssonjohan@outlook.com>
2016-01-29 16:06:35 +0100

    Creating Document Fragment, Content and set html via innerHTML, actual modal elm and set our options :beer:

--- js/cm-lightbox.js
+++ js/cm-lightbox.js
@@ -12,7 +12,7 @@
             className: 'fade-and-drop',
             closeButton: true,
             content: "",
-            mixWidth: 600,
+            maxWidth: 600,
             minWidth: 280,
             overlay: true
         };
@@ -22,6 +22,59 @@
             self.options = extendDefaults(defaults, arguments[0]);
         }
     }
+
+
+    // Private Methods
+
+    function buildOut() {
+        var content, contentHolder, docFrag;
+
+        // If content is an HTML string, append the html string.
+        // If content is a domNode, append its content.
+
+        if(typeof self.options.content === 'string') {
+            content = self.options.content;
+        } else {
+            content = self.options.content.innerHTML;
+        }
+
+        // Create a Documentframent to build with
+        docFrag = document.createDocumentFragment();
+
+        // Create modal element.
+        self.modal                  = document.createElement('div');
+        self.modal.className        = 'cm-modal ' + self.options.className;
+        self.modal.style.minWidth   = self.options.minWidth + 'px';
+        self.modal.style.maxWidth   = self.options.maxWidth + 'px';
+
+        // if closeButton option true, add close button
+        if(self.options.closeButton === true) {
+            self.closeButton = document.createElement('button');
+            self.closeButton.className = 'cm-modal-close close-button';
+            self.closeButton.innerHTML = 'X';
+            this.modal.appendChild(self.closeButton);
+        }
+
+        // if overlay is true, then we add it!
+        if(self.options.overlay === true) {
+            self.overlay = document.createElement('div');
+            self.overlay.className = 'cm-modal-overlay ' + self.options.classname;
+            docFrag.appendChild(self.overlay);
+        }
+
+        // Create content area and append it to modal
+        contentHolder = document.createElement('div');
+        contentHolder.className = 'cm-modal-content';
+        contentHolder.innerHTML = content;
+        self.modal.appendChild(contentHolder);
+
+        // Append modal to docFrag
+        docFrag.appendChild(self.modal);
+
+        // Append to body
+        document.body.appendChild(docFrag);
+    }
+
 
     // Utility method to extend defaults with user options
     function extendDefaults(source, props) {
